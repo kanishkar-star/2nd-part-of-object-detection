5a8ea3a42a498cbac296e460c9c1c833e55b56ee
kanishkar-star <71880783+kanishkar-star@users.noreply.github.com>
2021-03-08 09:41:42 +0530

    Add files via upload

---
+++ books.js
@@ -0,0 +1,33 @@
+status = '';
+
+function preload(){
+    first_img = loadImage('books.jpg');
+}
+
+function setup(){
+    canvas = createCanvas(680, 500);
+    canvas.center();
+    objDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById('status').innerHTML = "Status = Detecting Objects";
+}
+
+function draw(){
+    image(first_img, 0, 0, 680, 500)
+}
+
+function modelLoaded(){
+    console.log('Model Loaded');
+    status = true;
+    objDetector.detect(first_img, gotResult);
+}
+
+function gotResult(error, result){
+    if(error){
+        console.error(error);
+    }
+    console.log(result);
+}
+
+function back(){
+    window.location = 'home.html'
+}
---
+++ home.js
@@ -0,0 +1,15 @@
+function first(){
+    window.location = 'big_bottles.html'
+}
+
+function second(){
+    window.location = 'books.html'
+}
+
+function third(){
+    window.location = 'bottles.html'
+}
+
+function fourth(){
+    window.location = 'comp.html'
+}
